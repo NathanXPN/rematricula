d6d1ba9484e471681a361ae2af27be55c87dea2d
Vylgur <nathanbastos1989@gmail.com>
2024-08-01 02:38:02 -0300

    rematricula

---
+++ public/scripts.js
@@ -0,0 +1,60 @@
+// script.js
+document.querySelector('.prev').addEventListener('click', () => {
+    document.querySelector('.carousel').scrollBy({
+        left: -200,
+        behavior: 'smooth'
+    });
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    document.querySelector('.carousel').scrollBy({
+        left: 200,
+        behavior: 'smooth'
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('/clicks')
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll('.card').forEach(card => {
+                const productId = card.getAttribute('data-product-id');
+                const clickCountElem = card.querySelector('.click-count');
+                const clickLimit = parseInt(card.getAttribute('data-click-limit'));
+
+                if (data[productId]) {
+                    clickCountElem.textContent = data[productId];
+                    if (data[productId] >= clickLimit) {
+                        card.querySelector('button.matricular').disabled = true;
+                    }
+                }
+            });
+        });
+});
+
+function handleClick(button, url) {
+    const card = button.closest('.card');
+    const productId = card.getAttribute('data-product-id');
+    const clickCountElem = card.querySelector('.click-count');
+    const clickLimit = parseInt(card.getAttribute('data-click-limit'));
+
+    fetch('/click', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ productId }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            const clickCount = data.clickCount;
+            clickCountElem.textContent = clickCount;
+
+            if (clickCount >= clickLimit) {
+                button.disabled = true;
+            }
+
+            window.location.href = url;
+        });
+}
+
